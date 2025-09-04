6beaa5ff179f3985e7478feae0e76bd8fa246428
Taiwosamuel8364 <taiwosamuel8364@gmail.com>
2025-09-04 06:55:48 -0700

    add delete ticket, disable and enable ticket functionlity

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -2,7 +2,8 @@ const Ticket = require('../models/Ticket');
 
 const createTicket = async (req, res) => {
  try {
-    const { eventId, ticketName, ticketPrice, venueCapacity, endDate } = req.body;
+    const { ticketName, ticketPrice, venueCapacity, endDate } = req.body;
+    const eventId = req.params.eventId;
 
     const newTicket = await Ticket.create({
         eventId,
@@ -19,8 +20,65 @@ const createTicket = async (req, res) => {
  } catch (err) {
     res.status(500).json({ error: 'Server error' });
  }
-}
+};
+
+//disable ticket
+const disableTicket = async (req, res) => {
+    try {
+        const ticket = await Ticket.findById(req.params.ticketId);
+        if (!ticket) {
+            return res.status(404).json({ error: "Ticket not found" });
+        }
+
+        ticket.disabled = true;
+        await ticket.save();
+    } catch (err) {
+        return res.status(500).json({
+            error: "Internal server error"
+        });
+    }
+};
+
+//delete ticket
+const deleteTicket = async (req, res) => {
+    try {
+        const ticket = await Ticket.findById(req.params.ticketId);
+        if (!ticket) {
+            return res.status(404).json({ error: "Ticket not found" });
+        }
+
+        await ticket.remove();
+        res.status(200).json({ message: "Ticket deleted successfully" });
+    } catch (err) {
+        return res.status(500).json({
+            error: "Internal server error"
+        });
+    }
+};
+
+//enable ticket
+const enableTicket = async (req, res) => {
+    try {
+        const ticket = await Ticket.findById(req.params.ticketId);
+        if (!ticket) {
+            return res.status(404).json({ error: "Ticket not found" });
+        }
+
+        ticket.disabled = false;
+        await ticket.save();
+        res.status(200).json({ message: "Ticket enabled successfully" });
+    } catch (err) {
+        return res.status(500).json({
+            error: "Internal server error"
+        });
+    }
+};
+
+
 
 module.exports = {
-  createTicket
+  createTicket,
+  disableTicket,
+  deleteTicket,
+  enableTicket
 };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ticketController = require('../controllers/ticketController');
+const { organizerCheck } = require('../middlewares/requireAuth');
 
-router.post('/createTicket', ticketController.createTicket);
+router.use(organizerCheck);
+
+router.post('/createTicket/:eventId', ticketController.createTicket);
+router.patch('/disableTicket/:ticketId', ticketController.disableTicket);
+router.patch('/enableTicket/:ticketId', ticketController.enableTicket);
+router.delete('/deleteTicket/:ticketId', ticketController.deleteTicket);
 
 module.exports = router;
